680640267afae1d678f396713411e75fe4b037dd
jmskeoch <jmskeoch@yahoo.com>
2020-11-05 18:47:14 +0000

    Complete all methods
    - countOdd
    - checkDuplicates
    - convertArrayList

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,20 +4,30 @@ public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int count = 0;
+        for(int i = 0; i < list.size(); i++) {
+            count += (list.get(i) % 2 != 0) ? 1 : 0;
+        }
+        return count;
     }
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
+        for(int item: list1) {
+            for(int item2: list2) {
+                if(item == item2) {return true;}
+            }
+        }
         return false;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> x = new ArrayList<Integer>();
+        for(int i = 0; i < arr.length; i++) {
+            x.add(arr[i]);
+        }
+        return x;
     }
 
 
